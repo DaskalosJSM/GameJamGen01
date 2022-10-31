e289ffcdff8070dc30fa17ca19e75c2cc67d8bc3
DaskalosJSM <116764636+DaskalosJSM@users.noreply.github.com>
2022-10-31 10:56:11 -0500

    level2

--- Assets/Scripts/UIscripts/PlayerStats.cs
+++ Assets/Scripts/UIscripts/PlayerStats.cs
@@ -9,10 +9,12 @@ public class PlayerStats : MonoBehaviour
     public GameManager Manager;
     public GameObject Player;
     public Transform spawn;
+    public Transform spawn2;
     public int coins = 0;
     public int stars = 0;
     public int vidas = 3;
     private Rigidbody rb;
+    public bool Checkpoint = false;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -31,13 +33,26 @@ public class PlayerStats : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
 
-        if (other.gameObject.tag == "DeathZone")
+        if (other.gameObject.tag == "DeathZone"&& Checkpoint== false)
         {
             vidas -= 1;
             vidaui[vidaActual].gameObject.SetActive(false);
             Player.transform.position = spawn.transform.position;
             vidaActual -= 1;
+            rb.velocity= new Vector3(0,0,0);
+        }
+        if (other.gameObject.tag == "DeathZone"&& Checkpoint== true)
+        {
+            vidas -= 1;
+            vidaui[vidaActual].gameObject.SetActive(false);
+            Player.transform.position = spawn2.transform.position;
+            vidaActual -= 1;
+            rb.velocity= new Vector3(0,0,0);
         }
     }
-
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Respawn"){
+            Checkpoint= true;
+        }
+    }
 }
